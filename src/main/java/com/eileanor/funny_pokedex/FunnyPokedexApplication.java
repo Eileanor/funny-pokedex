f6f6d4f63ff31cd96f90f6c072373569eddfa28e
Eleonora Moschen <eleonora@192.168.1.50>
2026-06-24 12:02:44 +0200

    chore: scaffold Spring Boot project from Initialiazr

---
+++ src/main/java/com/eileanor/funny_pokedex/FunnyPokedexApplication.java
@@ -0,0 +1,13 @@
+package com.eileanor.funny_pokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunnyPokedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunnyPokedexApplication.class, args);
+	}
+
+}
